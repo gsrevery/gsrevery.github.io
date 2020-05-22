0f397c27286961090d14e015591d4053b033b97c
gaopan <1780171054@qq.com>
2020-05-22 11:08:49 +0800

    22

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: '学而时习之不亦说乎，有朋至远方来不亦乐乎。',
 	base: '/blog/',
 	head: ['link', { rel: 'icon', href: '/logo.png' }],
+	repo: 'git@github.com:gsrevery/gsrevery.github.io.git',
 	port: 1314,
 	markdown: {
         lineNumbers: true
