113347fee477888fcff6a0cc42bdc2a5207b9be6
gaopan <1780171054@qq.com>
2020-05-22 14:19:06 +0800

    上传打包文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     title: "gp's blog",
 	description: '学而时习之不亦说乎，有朋至远方来不亦乐乎。',
-	dest: './output', // 打包文件
-	base: '/output/',
+	dest: './blog', // 打包文件
+	base: '/blog/',
 	// head: ['link', { rel: 'icon', href: '/logo.png' }],
 	repo: 'git@github.com:gsrevery/gsrevery.github.io.git',
 	port: 1314,
